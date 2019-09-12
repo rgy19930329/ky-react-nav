b4c2e5e269928c9a90ba65883d6928a495618e54
冉光宇 <593289488@qq.com>
2019-09-12 18:07:52 +0800

    添加搜索

--- app/pages/home/components/Classify/detail.jsx
+++ app/pages/home/components/Classify/detail.jsx
@@ -9,22 +9,54 @@ import React from "react";
 import Title from "../Title";
 import LinkCard from "../LinkCard";
 import FolderCard from "../FolderCard";
-import { Tree, Spin } from "antd";
+import { Tree, Spin, Input } from "antd";
 import { get } from "@utils/wdio";
 
 const { TreeNode, DirectoryTree } = Tree;
+const { Search } = Input;
 const ref = new Wilddog("https://kylin.wilddogio.com/bookmarks");
+
+let dataList = []; // 数据源
+
+const generateList = data => {
+  for (let i = 0; i < data.length; i++) {
+    const node = data[i];
+    const { key } = node;
+    dataList.push({ key, title: key });
+    if (node.children) {
+      generateList(node.children);
+    }
+  }
+};
+
+const getParentKey = (key, tree) => {
+  let parentKey;
+  for (let i = 0; i < tree.length; i++) {
+    const node = tree[i];
+    if (node.children) {
+      if (node.children.some(item => item.key === key)) {
+        parentKey = node.key;
+      } else if (getParentKey(key, node.children)) {
+        parentKey = getParentKey(key, node.children);
+      }
+    }
+  }
+  return parentKey;
+};
 
 export default class ClassifyDetail extends React.Component {
 
 	state = {
 		treeData: [],
 		expandedKeys: [],
+		autoExpandParent: true,
+		searchValue: "",
 		loaded: false,
 	};
 
 	componentDidMount() {
 		get(ref, (treeData) => {
+			generateList(treeData);
 			this.setState({
 				treeData: treeData || [],
 				expandedKeys: ["root"],
@@ -34,12 +66,34 @@ export default class ClassifyDetail extends React.Component {
 	}
 
 	renderTreeNodes = (data) => {
+		const { searchValue, expandedKeys, autoExpandParent } = this.state;
+		console.log(searchValue)
 		return data.map(item => {
+			// const addTitle = (
+			// 	<FolderCard
+			// 		isRoot={item.key === "root"}
+			// 	>
+			// 		{item.title}
+			// 	</FolderCard>
+			// );
+			const index = item.title.indexOf(searchValue);
+			const beforeStr = item.title.substr(0, index);
+			const afterStr = item.title.substr(index + searchValue.length);
+			const title =
+          index > -1 ? (
+            <span>
+              {beforeStr}
+              <span style={{ color: "#f50" }}>{searchValue}</span>
+              {afterStr}
+            </span>
+          ) : (
+            <span>{item.title}</span>
+					);
 			const addTitle = (
 				<FolderCard
 					isRoot={item.key === "root"}
 				>
-					{item.title}
+					{title}
 				</FolderCard>
 			);
 			if (item.children) {
@@ -62,7 +116,7 @@ export default class ClassifyDetail extends React.Component {
 									url={item.url}
 									simple
 								>
-									{item.title}
+									{title}
 								</LinkCard>
 							) : addTitle
 					}
@@ -73,21 +127,47 @@ export default class ClassifyDetail extends React.Component {
 		});
 	};
 
+	onChange = e => {
+		const { value } = e.target;
+		const { treeData } = this.state;
+    const expandedKeys = dataList
+      .map(item => {
+        if (item.title.indexOf(value) > -1) {
+          return getParentKey(item.key, treeData);
+        }
+        return null;
+      })
+      .filter((item, i, self) => item && self.indexOf(item) === i);
+    this.setState({
+      expandedKeys,
+      searchValue: value,
+      autoExpandParent: true,
+    });
+  };
+
 	render() {
 		return (
 			<div className="comp-classify">
 				<Title icon="apartment">分类</Title>
 				<div className="comp-classify-inner">
+					<Search
+						style={{ marginBottom: 8, width: 300 }}
+						placeholder="Search"
+						onChange={this.onChange}
+					/>
 					<Spin spinning={!this.state.loaded}>
-					<DirectoryTree
-						multiple
-						expandedKeys={this.state.expandedKeys}
-						onExpand={(expandedKeys) => {
-							this.setState({ expandedKeys });
-						}}
-					>
-						{this.renderTreeNodes(this.state.treeData)}
-					</DirectoryTree>
+						<DirectoryTree
+							multiple
+							expandedKeys={this.state.expandedKeys}
+							onExpand={(expandedKeys) => {
+								this.setState({
+									expandedKeys,
+									autoExpandParent: false
+								});
+							}}
+						>
+							{this.renderTreeNodes(this.state.treeData)}
+						</DirectoryTree>
 					</Spin>
 				</div>
 			</div>
